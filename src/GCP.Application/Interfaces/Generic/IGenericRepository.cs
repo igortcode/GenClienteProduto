b500d825489183c1d4839032574866f19298fbc0
IgorTorres <igor.torres@trustprev.com.br>
2024-05-25 11:28:25 -0300

    Adicionando a camada de application

---
+++ src/GCP.Application/Interfaces/Generic/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using GCP.Core.Entities.Generic;
+
+namespace GCP.Application.Interfaces.Generic
+{
+    public interface IGenericRepository<T> where T : Entity
+    {
+        int Add(T entity);
+        int Update(T entity);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        int Remove(int id);
+    }
+}
